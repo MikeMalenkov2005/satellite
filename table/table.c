8f6990fb3e8b951827ed96ced36b9386327cd077
MikeMalenkov2005 <mike.malenkov@gmail.com>
2022-02-18 18:34:08 +0300

    Compleated table.h Implemented: tAdd, tSetK, tSetI

--- table/table.c
+++ table/table.c
@@ -1,4 +1,6 @@
 #include "table.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 struct node {
 	size_t key;
@@ -10,3 +12,70 @@ typedef struct table {
 	size_t size;
 	struct node *first;
 } Table;
+
+struct node* newNode(size_t key, void *val) {
+	struct node *node = (struct node*) malloc(sizeof(struct node));
+	node -> key = key;
+	node -> val = val;
+	node -> next = NULL;
+	return node;
+}
+
+struct node* freeNode(struct node *node) {
+	struct node *next = node -> next;
+	free((void*)node);
+	return next
+}
+
+Table* newTable() {
+	Table *table = (Table*) malloc(sizeof(Table));
+	table -> size = 0;
+	table -> node = NULL;
+}
+
+void freeTable(Table *table) {
+	struct node *node = table -> first
+	free((void*)table);
+	while (node != NULL) {
+		node = freeNode(node);
+	}
+}
+
+void tAdd(Table *table, void *val) {
+	struct node *node = table -> first;
+	while (node -> next != NULL) node = node -> next;
+	node -> next = newNode(0, val);
+	table -> size++;
+}
+
+void* tSetK(Table *table, size_t key, void *val) {
+	struct node *node = table -> first;
+	while (node -> next != NULL) {
+		if (node -> key == key) {
+			void *prev = node -> val;
+			node -> val = val;
+			return prev;
+		}
+		node = node -> next;
+	}
+	node -> next = newNode(key, val);
+	table -> size++;
+	return NULL;
+}
+
+void* tSetI(Table *table, size_t index, void *val) {
+	if (index > table -> size) {
+		fprintf(stderr, "Runtime index out of range exception!!!\n");
+		exit(-1);
+	}
+	else if (index == table -> size) {
+		tAdd(table, val);
+	}
+	struct node *node = table -> first;
+	for (size_t i = 0; i < index; i++) {
+		node = node -> next;
+	}
+	void *prev = node -> val;
+	node -> val = val;
+	return prev;
+}
--- table/table.h
+++ table/table.h
@@ -6,4 +6,14 @@ typedef struct table {
 	size_t size;
 } Table;
 
+Table* newTable();
+void freeTable();
+void tAdd(Table *table, void *val);
+void* tSetK(Table *table, size_t key, void *val);
+void* tSetI(Table *table, size_t index, void *val);
+void* tGetK(Table *table, size_t key);
+void* tGetI(Table *table, size_t index);
+void* tRemoveK(Table *table, size_t key);
+void* tRemoveI(Table *table, size_t index);
+
 #endif
